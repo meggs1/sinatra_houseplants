d3869692c893db04da652dfbd39f3160ede66997
meggs1 <megan.schertz@gmail.com>
2021-02-03 00:22:38 -0600

    add user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,39 @@
-get '/signup' do 
-    redirect_if_logged_in
-    erb :"users/new"
-end
+class UsersController < ApplicationController
 
-post '/signup' do
-    user = User.new(params)
-    if user.save
-        session[:user_id] = user.id
-        redirect "/rooms"
-    else
-        #add flash error message
-        redirect "/signup"
+    get '/signup' do 
+        redirect_if_logged_in
+        erb :"users/new"
     end
-end
 
-get '/login' do #render login form
-    redirect_if_logged_in
-    erb :"users/login"
-end
-
-post '/login' do #creates session
-    user = User.find_by(:username => params["username"])
-    if user && user.authenticate(params["password"])
-        session["user_id"] = user.id #logs user in and sets session id
-        redirect "/rooms"
-    else
-        redirect "/login"
+    post '/signup' do
+        user = User.new(params)
+        if user.save
+            session[:user_id] = user.id
+            redirect "/rooms"
+        else
+            #add flash error message
+            redirect "/signup"
+        end
     end
-end
 
-delete '/logout' do #destroys session
-    session.clear
-    redirect "/"
+    get '/login' do #render login form
+        redirect_if_logged_in
+        erb :"users/login"
+    end
+
+    post '/login' do #creates session
+        user = User.find_by(:username => params["username"])
+        if user && user.authenticate(params["password"])
+            session["user_id"] = user.id #logs user in and sets session id
+            redirect "/rooms"
+        else
+            redirect "/login"
+        end
+    end
+
+    delete '/logout' do #destroys session
+        session.clear
+        redirect "/"
+    end
+
 end
