3baa31bc00ed50fce94524b2c80ddce5eea96a32
meggs1 <megan.schertz@gmail.com>
2021-02-03 00:10:34 -0600

    add instance, helper and private methods to appplication controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+class ApplicationController < Sinatra::Base
+    
+    configure do 
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, ENV['SESSION_SECRET']
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    helpers do #in controller because i'm going to use them in my views
+        def current_user
+            User.find_by_id(session["user_id"]) if session["user_id"] #returns nil (.find returns false)
+        end
+
+        def logged_in?
+            !!current_user #if the current user exists
+        end
+    end
+
+    private
+
+    def redirect_if_not_logged_in #added here to make code more dry
+        redirect "/" if !logged_in? 
+    end
+
+    def redirect_if_logged_in #added here to make code more dry
+        redirect "/rooms" if logged_in?
+    end
+    
+end
