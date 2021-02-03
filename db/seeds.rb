196df3a6ee8bdb11825f920772976680d7a05d38
meggs1 <megan.schertz@gmail.com>
2021-02-03 00:16:40 -0600

    create seed data

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+
+User.create!([
+    {name: "Megan" , username: "meggs", password: "password"},
+    {name: "Sebastian" , username: "seabass", password: "password"},
+    {name: "Arya", username: "miss", password: "password"},
+    {name: "Jasper" , username: "boo", password: "password" }
+])
+
+Plant.create!([
+    {name: "Snake Plant", nickname: "Richard", sunlight: "Moderate", water: "Once every two weeks", fertilizer: "Yearly", notes: "dry"},
+    {name: "Snake Plant", nickname: "Sunny", sunlight: "Moderate", water: "Once every two weeks", fertilizer: "Yearly",notes: "thriving"},
+    {name: "Monstera", sunlight: "Direct", water: "Once every week", fertilizer: "Every 6 months"}
+])
