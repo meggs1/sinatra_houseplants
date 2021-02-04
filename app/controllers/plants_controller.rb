0fb6925a4742a7901f2acb8f48a421d9a8f72ead
meggs1 <megan.schertz@gmail.com>
2021-02-03 19:30:23 -0600

    remove placeholders from plant edit form

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -39,8 +39,8 @@ class PlantsController < ApplicationController
 
     patch '/plants/:id' do
         redirect_if_not_logged_in
-        plant = Plant.find_by(id: params[:id])
-        plant.update(params["plant"])
+        @plant = Plant.find_by_id(params[:id])
+        @plant.update(params["plant"])
         redirect "/plants/#{@plant.id}"
     end
 
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,16 +2,4 @@ class Plant < ActiveRecord::Base
     belongs_to :user
 
     validates :name, presence: true
-    validates :nickname, presence: true, uniqueness: { case_sensitive: false }
-
-    def slug
-        self.nickname.downcase.gsub(" ","-")
-    end
-
-    def self.find_by_slug(slug)
-        self.all.find{|item| item.slug == slug}
-    end
-
 end
-
-
