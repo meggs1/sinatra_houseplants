2b54dc55301c0f0bf16772afeb745d14d4f68ae3
meggs1 <megan.schertz@gmail.com>
2021-02-24 01:59:02 -0600

    make plant redirects more functional

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,6 +5,7 @@ class PlantsController < ApplicationController
     end
 
     get '/plants' do
+        redirect_if_not_logged_in
         @user = User.find(session["user_id"])
         @plants = @user.plants
         erb :"plants/index"
